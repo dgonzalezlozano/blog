9534c97342bb6fea81c86296665197b96b6f337e
Domingo Gonzalez <domingo.gonzalez@eoniantec.com>
2024-08-15 22:55:29 +0200

    Base updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://dgonzalezlozano.github.io',
-  base: 'domin',
+  base: 'blog',
 });
