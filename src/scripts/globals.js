74c48282d99aa0da7ddc7191857a64889151ad40
Domingo Gonzalez <domingo.gonzalez@eoniantec.com>
2024-09-17 00:11:16 +0200

    mobile navigation fixed

--- src/scripts/globals.js
+++ src/scripts/globals.js
@@ -1,3 +1,3 @@
 export const SITE_TITLE = "domin.es";
-export const SITE_DESCRIPTION = "Este es un blog increíble hecho con Astro.";
+export const SITE_DESCRIPTION = "blog de domin";
 export const CONTACT_EMAIL = "hola@domin.es";
